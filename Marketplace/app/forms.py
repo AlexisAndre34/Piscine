b0a0cbe00ae2777b984bc01b33555a5b577892ea
GAzerty <guilherme.t14@gmail.com>
2018-11-03 14:38:46 +0100

    Creation de views et de formulaire pour le login et le signup client

--- Marketplace/app/forms.py
+++ Marketplace/app/forms.py
@@ -1,2 +1,26 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+#Formulaire pour la connexion au site
+class SignInForm(forms.Form):
+    username = forms.CharField(max_length=254)
+    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
+
+
+#-----Formulaire de creation ---
+
+#Formulaire de creation d'un compte client
+class SignUpFormClient(UserCreationForm):
+    datenaissanceclient = forms.DateField()
+    telephoneclient = forms.CharField()
+    codepostalclient = forms.IntegerField()
+    villeclient = forms.CharField()
+    rueclient = forms.CharField()
+    class Meta:
+        model = User
+        fields = ('username','first_name','last_name','datenaissanceclient','email','telephoneclient','codepostalclient','villeclient','rueclient','password1', 'password2', )
+
+
+
+#-----Formulaire de mise à jour ---
--- Marketplace/app/views.py
+++ Marketplace/app/views.py
@@ -1,8 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User, Group
+from app.models import Client
+from app.forms import SignInForm, SignUpFormClient
+
+#VIEW PAGE D'ACCUEIL
+def homepage(request):
+    return render(request, 'index.html')
+
+
+#---------------- VIEWS DE CONNEXION, DECONNEXION  ----------------
+
+
+def login_user(request):
+    error=False
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            raw_password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=raw_password)
+            if user:
+                login(request,user)
+                #request.session['test']="test session réussit"
+                return redirect('homepage')
+            else:
+                error=True
+                #ErrorMessage = "Username ou mot de passe incorrect"
+    else:
+        form= SignInForm()
+    return render(request, 'signin.html', locals())
+
+
+def logout_user(request):
+    logout(request)
+    return render(request, 'index.html')
 
 
 #---------------- VIEWS DE CREATION (CREATE) ----------------
 
+#Creation d'un compte client
+def signup_client(request):
+    if request.method == 'POST':
+        form = SignUpFormClient(request.POST)
+        if form.is_valid():
+            form.save() #Sauvegarde/Creation d'un utilisateur de base
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password) #Authentification de l'utilisateur
+            Utilisateur = User.objects.get(username=username)
+            groupe_client = Group.objects.get(id='2')  # On ajoute l'utilisateur au groupe client ici (id groupe client = 2 )
+            Utilisateur.groups.add(groupe_client)
+            client = Client(numclient=Utilisateur, datenaissanceclient=form.cleaned_data.get('datenaissanceclient'), telephoneclient=form.cleaned_data.get('telephoneclient'), codepostalclient=form.cleaned_data.get('codepostalclient'), villeclient=form.cleaned_data.get('villeclient'), rueclient=form.cleaned_data.get('rueclient'), numclient_id=Utilisateur.id)
+            client.save()  # Sauvegarde du client
+            login(request, user) #Connexion au site
+            return redirect('homepage')
+    else:
+        form = SignUpFormClient(request.POST)
+    return render(request, 'signup_client.html', {'formClient': form})
 
 
 
