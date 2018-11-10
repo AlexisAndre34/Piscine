9cb94809ffeb544f2896be9cae3e0b46e2c988fd
GAzerty <guilherme.t14@gmail.com>
2018-11-10 17:49:40 +0100

    ajout des fichiers css et js correspondant au framework Materialize

--- Marketplace/static/js/script_marketplace.js
+++ Marketplace/static/js/script_marketplace.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $('select').formSelect();
   });
+  
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
+
+$(".dropdown-trigger").dropdown();
